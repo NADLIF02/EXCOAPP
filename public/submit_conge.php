919b043c81e1f2f94e43bf50c785ea3299de86db
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 14:39:24 +0200

    Create submit_conge.php

---
+++ public/submit_conge.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+$mysqli = new mysqli("localhost", "username", "password", "database_name"); // Modifiez avec vos vrais données
+
+$start_date = $_POST['start_date'];
+$end_date = $_POST['end_date'];
+$description = $_POST['description'];
+
+$query = "INSERT INTO conges (start_date, end_date, description) VALUES (?, ?, ?)";
+$stmt = $mysqli->prepare($query);
+$stmt->bind_param("sss", $start_date, $end_date, $description);
+if ($stmt->execute()) {
+    echo json_encode(['success' => true]);
+} else {
+    echo json_encode(['success' => false]);
+}
+
+$mysqli->close();
+?>
