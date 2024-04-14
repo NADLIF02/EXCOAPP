100d04f83e18cb0f4a1f5e8408518be47384a0a7
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 19:07:41 +0200

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,11 +1,16 @@
 <?php
 session_start();
-// db.php - Gérer la connexion à la base de données
-$host = 'db';  // Utiliser 'db' qui est le nom du service Docker pour MySQL
-$username = 'admin';  // Nom d'utilisateur pour MySQL
-$password = 'admin123';  // Mot de passe pour MySQL
-$database = 'employee_leaves';  // Nom de la base de données
-$mysqli = new mysqli("db", "admin", "admin123", "employee_leaves");
+
+// Récupérer les paramètres de connexion de variables d'environnement
+$host = getenv('DB_HOST') ?: 'db';  // Utiliser 'db' comme valeur par défaut si non défini
+$username = getenv('DB_USER') ?: 'admin';  // Nom d'utilisateur par défaut
+$password = getenv('DB_PASS') ?: 'admin123';  // Mot de passe par défaut
+$database = getenv('DB_NAME') ?: 'employee_leaves';  // Nom de la base de données par défaut
+
+// Créer une connexion à la base de données
+$mysqli = new mysqli($host, $username, $password, $database);
+
+// Vérifier les erreurs de connexion
 if ($mysqli->connect_error) {
     die('Erreur de connexion : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
