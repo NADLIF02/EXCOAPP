ceee74ccb7abcd94e1b5cb3111c0a9c44876dbe3
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 19:28:34 +0200

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,10 +1,8 @@
 <?php
-// db.php - Gérer la connexion à la base de données avec PDO
-$host = 'db'; // Utiliser 'db' qui est le nom du service Docker pour MySQL
-$username = 'admin'; // Nom d'utilisateur pour MySQL
-$password = 'admin123'; // Mot de passe pour MySQL
-$database = 'employee_leaves'; // Nom de la base de données
+$host = 'db';
+$username = 'admin';
+$password = 'admin123';
+$database = 'employee_leaves';
 
-$db = new PDO("mysql:host=$host;dbname=$database", $username, $password);
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$db = new PDO("mysql:host=$host;dbname=$database", $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
 ?>
