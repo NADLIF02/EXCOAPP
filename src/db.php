d6ab4345812d998bf913979c70714e8b9cff29a1
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 17:18:27 +0200

    Update db.php

--- src/db.php
+++ src/db.php
@@ -5,9 +5,7 @@ $host = 'db';  // Utiliser 'db' qui est le nom du service Docker pour MySQL
 $username = 'admin';  // Nom d'utilisateur pour MySQL
 $password = 'admin123';  // Mot de passe pour MySQL
 $database = 'employee_leaves';  // Nom de la base de données
-
-$mysqli = new mysqli($host, $username, $password, $database);
-
+$mysqli = new mysqli("db", "admin", "admin123", "employee_leaves");
 if ($mysqli->connect_error) {
     die('Erreur de connexion : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
 }
