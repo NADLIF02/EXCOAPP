02f32212d0d931691fe66310aae42c22a11b25ed
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 15:09:01 +0200

    Update load_events.php

--- public/load_events.php
+++ public/load_events.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+require_once '/var/www/src/db.php';
 // Connexion à la base de données
 $db = new PDO('mysql:host=localhost;dbname=nom_de_votre_base', 'utilisateur', 'mot_de_passe');
 $query = "SELECT id, username, title, start_date AS start, end_date AS end FROM conges";
