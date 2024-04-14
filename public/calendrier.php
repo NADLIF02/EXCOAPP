66ca30864a946db706ad3e03f354690c132c17a3
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 18:57:06 +0200

    Update calendrier.php

--- public/calendrier.php
+++ public/calendrier.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Calendrier Annuel des Congés</title>
+    <title>Calendrier des Congés</title>
     <link rel="stylesheet" href="styles.css">
     <link href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.10.2/fullcalendar.min.css' rel='stylesheet' />
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
@@ -19,10 +19,35 @@
                     center: 'title',
                     right: 'month,agendaWeek,agendaDay,listMonth'
                 },
+                defaultView: 'month',
+                navLinks: true, // can click day/week names to navigate views
                 editable: true,
-                events: 'load_events.php', // Modifier ceci pour correspondre à votre configuration
+                eventLimit: true, // allow "more" link when too many events
+                events: 'load_events.php',
                 eventRender: function(event, element) {
-                    element.find('.fc-title').append('<br/>' + event.title); // Ajouter le nom de l'utilisateur au titre de l'événement
+                    element.find('.fc-title').append('<br/><strong>' + event.title + '</strong>'); // Append user info
+                },
+                selectable: true,
+                selectHelper: true,
+                select: function(start, end) {
+                    var title = prompt('Entrez le motif de votre congé:');
+                    if (title) {
+                        var eventData = {
+                            title: title,
+                            start: start,
+                            end: end
+                        };
+                        $('#calendar').fullCalendar('renderEvent', eventData);
+                        // Save to server
+                        $.post('submit_conge.php', {
+                            title: title,
+                            start: start.format(),
+                            end: end.format()
+                        }, function(response) {
+                            console.log(response);
+                        });
+                    }
+                    $('#calendar').fullCalendar('unselect');
                 }
             });
         });
