a4d1ee1d29d8ca2081bb179de7bd35ae5c84b2d3
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 15:27:44 +0200

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,15 @@
+CREATE DATABASE IF NOT EXISTS employee_leaves;
+
+CREATE USER 'admin'@'%' IDENTIFIED BY 'admin123';
+GRANT ALL PRIVILEGES ON employee_leaves.* TO 'admin'@'%';
+FLUSH PRIVILEGES;
+
+USE employee_leaves;
+
 CREATE TABLE conges (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255),
-    start_date DATE,
-    end_date DATE,
+    username VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
     description TEXT
 );
