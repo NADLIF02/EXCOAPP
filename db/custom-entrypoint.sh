f5ded739e5acaa4c1876779bc63c3898b63ba26f
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 23:29:17 +0200

    Update custom-entrypoint.sh

--- db/custom-entrypoint.sh
+++ db/custom-entrypoint.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 set -e
 
-# Start the database server
-docker-entrypoint.sh mysqld &
+# Print out where mysqld and mysql_upgrade are located
+which mysqld
+which mysql_upgrade
 
-# Wait for the server to start (simple sleep for demo purposes, check for server readiness in production)
-sleep 10
-
-# Run mysql_upgrade
-mysql_upgrade -u root -p"$MYSQL_ROOT_PASSWORD"
-
-# Bring mysqld back to the foreground for Docker management
-wait $!
+# Rest of your script...
