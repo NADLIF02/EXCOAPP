737c33e425ad2cef72c819f413e999f0ddaeb8f3
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 15:15:38 +0200

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+require_once '/var/www/src/db.php';  // Chemin absolu
 // db.php - Gérer la connexion à la base de données
 $host = 'db';  // Utiliser 'db' qui est le nom du service Docker pour MySQL
 $username = 'username'; // Remplacer par votre nom d'utilisateur réel
