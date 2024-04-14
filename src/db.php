dfb8242aa0352bffe93435f53653c4623f44958c
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 19:44:45 +0200

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,8 +1,13 @@
 <?php
-$host = 'db';
-$username = 'admin';
-$password = 'admin123';
-$database = 'employee_leaves';
+$host = 'db';  // L'adresse du serveur de base de données
+$username = 'admin';  // Le nom d'utilisateur pour se connecter
+$password = 'admin123';  // Le mot de passe pour se connecter
+$database = 'employee_leaves';  // Le nom de la base de données
 
-$db = new PDO("mysql:host=$host;dbname=$database", $username, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+try {
+    $db = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
 ?>
