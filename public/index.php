76d6ed5f6fef79bfbe28c0128d17987e13864208
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 13:45:58 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -33,31 +33,34 @@ if (isset($_POST['login'])) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Accueil</title>
+    <title>EXCOAPP - Accueil</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <header>
-        <h1>Bienvenue sur notre site de gestion de congés!</h1>
-        <nav>
-            <ul>
-                <li><a href="index.php">Accueil</a></li>
-                <li><a href="calendrier.php">Calendrier</a></li>
-            </ul>
-        </nav>
-    </header>
-    <section>
-        <h2>Connexion</h2>
-        <?php if (!empty($error_message)): ?>
-            <p style='color: red;'><?= $error_message ?></p>
-        <?php endif; ?>
-        <form action="index.php" method="post">
-            <label for="username">Nom d'utilisateur:</label>
-            <input type="text" id="username" name="username" required><br>
-            <label for="password">Mot de passe:</label>
-            <input type="password" id="password" name="password" required><br>
-            <input type="submit" name="login" value="Se connecter">
-        </form>
-    </section>
+    <div class="container">
+        <header>
+            <h1>EXCOAPP</h1>
+            <p>Bienvenue sur notre site de gestion de congés!</p>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="calendrier.php">Calendrier</a></li>
+                </ul>
+            </nav>
+        </header>
+        <section>
+            <h2>Connexion</h2>
+            <?php if (!empty($error_message)): ?>
+                <p class="error"><?= $error_message ?></p>
+            <?php endif; ?>
+            <form action="index.php" method="post">
+                <label for="username">Nom d'utilisateur:</label>
+                <input type="text" id="username" name="username" required><br>
+                <label for="password">Mot de passe:</label>
+                <input type="password" id="password" name="password" required><br>
+                <input type="submit" name="login" value="Se connecter">
+            </form>
+        </section>
+    </div>
 </body>
 </html>
