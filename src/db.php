afefd9f38fa214062ccd66e9868676150721fd15
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 19:25:37 +0200

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,21 +1,10 @@
-
 <?php
-session_start();
-require_once '/var/www/src/db.php';  // Assurez-vous que le chemin d'accès est correct
-
-// Le reste de votre script...
-
 // db.php - Gérer la connexion à la base de données avec PDO
+$host = 'db'; // Utiliser 'db' qui est le nom du service Docker pour MySQL
+$username = 'admin'; // Nom d'utilisateur pour MySQL
+$password = 'admin123'; // Mot de passe pour MySQL
+$database = 'employee_leaves'; // Nom de la base de données
 
-$host = 'db';  // Utiliser 'db' qui est le nom du service Docker pour MySQL
-$username = 'admin';  // Nom d'utilisateur pour MySQL
-$password = 'admin123';  // Mot de passe pour MySQL
-$database = 'employee_leaves';  // Nom de la base de données
-
-try {
-    $db = new PDO("mysql:host=$host;dbname=$database;charset=utf8", $username, $password);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die('Erreur de connexion : ' . $e->getMessage());
-}
+$db = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
