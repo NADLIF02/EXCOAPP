4b5905903fced847e53f9ae08071377cc19e7842
FILDAN <152385343+NADLIF02@users.noreply.github.com>
2024-04-14 19:35:00 +0200

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,6 +1,8 @@
+-- Création de la base de données si elle n'existe pas
 CREATE DATABASE IF NOT EXISTS employee_leaves;
 USE employee_leaves;
 
+-- Création de la table des congés si elle n'existe pas
 CREATE TABLE IF NOT EXISTS conges (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
@@ -9,14 +11,18 @@ CREATE TABLE IF NOT EXISTS conges (
     description TEXT
 );
 
-CREATE USER 'admin'@'%' IDENTIFIED BY 'admin123';
-GRANT ALL PRIVILEGES ON employee_leaves.* TO 'admin'@'%';
-FLUSH PRIVILEGES;
+-- Création de la table des utilisateurs si elle n'existe pas
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
 
+-- Création d'un utilisateur de base de données spécifique pour l'application si non existant
+CREATE USER IF NOT EXISTS 'admin'@'%' IDENTIFIED BY 'admin123';
 
+-- Attribution des privilèges à l'utilisateur
+GRANT ALL PRIVILEGES ON employee_leaves.* TO 'admin'@'%';
 
+-- Application des changements de privilèges
+FLUSH PRIVILEGES;
